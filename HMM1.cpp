5ec30cf1bb67c31b7c7ca12e5dbd497533464d59
Tanay <tanay@kth.se>
2017-09-10 23:41:28 +0200

    HMM1 improved and made a lot shorter

--- HMM1.cpp
+++ HMM1.cpp
@@ -1,6 +1,4 @@
 #include "HMM_Matrix.hpp"
-#include "matrix_multi.hpp"
-
 
 // Alpha Pass
 
@@ -24,8 +22,8 @@ int main()
 //    cout<<"\nA_col:"<<a_col;
     it = numbers.begin();
     A.setHMMmatrix(numbers, a_row, a_col, it);
-//    cout<<"\nTransition Matrix A:\n";
-//    A.printHMMmatrix();
+    cout<<"\nTransition Matrix A:\n";
+    A.printHMMmatrix();
 
 
 
@@ -42,8 +40,8 @@ int main()
 //    cout<<"\nB_col:"<<b_col;
     it = numbers.begin();
     B.setHMMmatrix(numbers, b_row, b_col, it);
-//    cout<<"\nObservation Matrix B:\n";
-//    B.printHMMmatrix();
+    cout<<"\nObservation Matrix B:\n";
+    B.printHMMmatrix();
 
 //    // Matrix Pi
     numbers.clear();
@@ -58,8 +56,8 @@ int main()
 //    cout<<"\nPi_col:"<<pi_col;
     it = numbers.begin();
     Pi.setHMMmatrix(numbers, pi_row, pi_col, it);
-//    cout<<"\nInitial Pi:\n";
-//    Pi.printHMMmatrix();
+    cout<<"\nInitial Pi:\n";
+    Pi.printHMMmatrix();
 
     // Observation Sequence
     numbers.clear();
@@ -74,8 +72,8 @@ int main()
 //    cout<<"\nSeq_col:"<<seq_col;
     it = numbers.begin();
     Seq.setHMMmatrix(numbers, seq_row, seq_col, it);
-//    cout<<"\nObservation Sequence:\n";
-//    Seq.printHMMmatrix();
+    cout<<"\nObservation Sequence:\n";
+    Seq.printHMMmatrix();
 
 
 
@@ -87,62 +85,31 @@ int main()
     vector< vector<float> > b = B.getHMMmatrix();
     vector< vector<float> > pi = Pi.getHMMmatrix();
     vector< vector<float> > seq = Seq.getHMMmatrix();
-    vector< vector<float> > alpha;
-    vector< vector<float> > temp1;
-    vector< vector<float> > temp2;
-    int pos;
-    vector< vector<float> > b_trans = matrix_transpose(b);
-    HMMmatrix OB_SEQ;
+    vector< vector<float> > alpha (seq_col, vector<float>(a_col));
 
+    int observpos;
 
     // Alpha 0
-    it = seq[0].begin();
-    pos = *it;
-
-    it = b_trans[pos].begin();
-    OB_SEQ.setHMMmatrix(b_trans[pos], 1, b_col, it);
-//    cout<<"\n\nThe observation at "<<pos<<" is:\n";
-//    OB_SEQ.printHMMmatrix();
-
-    temp1 = OB_SEQ.getHMMmatrix();
-    alpha = element_matrix_multiply(pi, temp1);
-//    cout<<"\n\nAlpha at 0 :\n";
-//    vector_print(alpha);
-
-    temp1.clear();
-    OB_SEQ.clear_matrix();
+    observpos = seq[0][0];
+    for(unsigned int i = 0; i<a_col; i++)
+        alpha[0][i] = pi[0][i]*b[i][observpos];
 
     // Rest of the alpha
-    for(unsigned int i = 1; i<seq[0].size(); i++)
+    for(unsigned int ts = 1; ts<seq[0].size(); ts++)
     {
-        it = seq[0].begin() + i;
-        pos = *it;
+        observpos = seq[0][ts];
+        for(unsigned int i=0; i<a_col; i++)
+        {
+            for(unsigned int j = 0; j<a_col; j++)
+                alpha[ts][i] = alpha[ts][i] + alpha[ts-1][j]*a[j][i];
 
-        it = b_trans[pos].begin();
-        OB_SEQ.setHMMmatrix(b_trans[pos], 1, b_col, it);
-//        cout<<"\n\nThe observation at "<<pos<<" is:\n";
-//        OB_SEQ.printHMMmatrix();
-
-        temp1 = OB_SEQ.getHMMmatrix();
-        temp2 = cross_matrix_multiply(alpha, a);
-
-        // Next Alpha
-        alpha = element_matrix_multiply(temp2, temp1);
-
-//        cout<< "\n\nAlpha at "<<i<<" :\n\n";
-//        vector_print(alpha);
-
-        temp1.clear();
-        temp2.clear();
-        OB_SEQ.clear_matrix();
-
-
+            alpha[ts][i] = alpha[ts][i]*b[i][observpos];
+        }
     }
 
     float sum = 0;
-    for(unsigned int i=0; i<alpha.size(); i++)
-        for(unsigned int j=0; j<alpha[0].size(); j++)
-            sum = sum + alpha[i][j];
+    for(unsigned int i=0; i<alpha[0].size(); i++)
+        sum += alpha[seq_col-1][i];
 
     cout<<"\nThe probability of the given sequence for the given model is: "<<sum;
     cout<<"\n";
